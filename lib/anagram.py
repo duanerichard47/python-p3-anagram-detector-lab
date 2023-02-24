58f9b325f9aebf472b2fbaa19c2ae8bf5ef3ffbe
Duane <duanerichard47@hotmail.com>
2023-02-24 14:06:25 -0500

    not complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self,word):
+        self.word = word
+        pass
+
+    def match(self,word, a_list):
+        word_backwards = word[::-1]
+        self.a_list = []
+        if word_backwards in  self.a_list: 
+        return a_list
